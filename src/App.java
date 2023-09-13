b60261806ca14f5f66297d69b6287b62fd6d24f5
Ruben Wilkenson <ruben.wilkenson@elev.ga.ntig.se>
2023-09-13 10:06:34 +0200

    skapat kvot program

--- src/App.java
+++ src/App.java
@@ -1,5 +1,24 @@
+import java.util.Scanner;
+
+
 public class App {
+    String tal_1;
+    String tal_2;
+
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        App var = new App();
+
+        Scanner tan = new Scanner(System.in);
+
+        System.out.print("Ange tal:     ");
+        var.tal_1 = tan.nextLine();
+
+        System.out.print("Ange tal:     ");
+        var.tal_2 = tan.nextLine();
+
+        System.out.println(Double.parseDouble(var.tal_1) / Double.parseDouble(var.tal_2));
+
+        
+
     }
 }
