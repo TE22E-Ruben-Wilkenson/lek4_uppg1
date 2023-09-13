e4202beb7b2c9a6a0eecc9bd7cc37c35348eb123
Ruben Wilkenson <ruben.wilkenson@elev.ga.ntig.se>
2023-09-13 10:10:44 +0200

    Skapat b

--- src/App.java
+++ src/App.java
@@ -5,6 +5,9 @@ public class App {
     String tal_1;
     String tal_2;
 
+    String substantiv;
+    String plural;
+
     public static void main(String[] args) throws Exception {
         App var = new App();
 
@@ -16,9 +19,17 @@ public class App {
         System.out.print("Ange tal:     ");
         var.tal_2 = tan.nextLine();
 
-        System.out.println(Double.parseDouble(var.tal_1) / Double.parseDouble(var.tal_2));
+        System.out.println("kvoten är " + Double.parseDouble(var.tal_1) / Double.parseDouble(var.tal_2));
 
-        
+
+        System.out.print("Ange ett substantiv:  ");
+        var.substantiv = tan.nextLine();
+
+        System.out.print("Ange substantivets pluraländelse:  ");
+        var.plural = tan.nextLine();
+
+        System.out.println("En " + var.substantiv + " Flera " + (var.substantiv + var.plural));
+
 
     }
 }
